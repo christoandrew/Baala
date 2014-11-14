d031ac6f0e0ae05baee388e1bc3f85e2a4a2d10f
Christo Andrew <andrew.christoandrew.christo@gmail.com>
2014-11-14 21:25:29 +0300

    The custom list adapter shows images in the bar list

---
+++ app/src/main/java/com/iconasystems/christo/utils/BarListAdapter.java
@@ -0,0 +1,111 @@
+package com.iconasystems.christo.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.iconasystems.christo.baalafinal.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Christo on 11/14/2014.
+ */
+public class BarListAdapter extends BaseAdapter {
+    private static final String TAG_BAR_ID = "bar_id";
+    private final Activity activity;
+    private final ArrayList<HashMap<String, String>> barsList;
+    private LayoutInflater inflater;
+    private ImageView mBarImage;
+    private TextView mBarId;
+
+    private static final String TAG_DRINK_IMAGE = "bar_image";
+
+    public BarListAdapter(Activity activity, ArrayList<HashMap<String, String>> barsList) {
+        this.activity = activity;
+        this.barsList = barsList;
+    }
+
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of items.
+     */
+    @Override
+    public int getCount() {
+        return barsList.size();
+    }
+
+    /**
+     * Get the data item associated with the specified position in the data set.
+     *
+     * @param position Position of the item whose data we want within the adapter's
+     *                 data set.
+     * @return The data at the specified position.
+     */
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    /**
+     * Get the row id associated with the specified position in the list.
+     *
+     * @param position The position of the item within the adapter's data set whose row id we want.
+     * @return The id of the item at the specified position.
+     */
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link android.view.LayoutInflater#inflate(int, android.view.ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view  = convertView;
+
+        if (inflater == null)
+            inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (view == null) view = inflater.inflate(R.layout.bar_list_item, null);
+
+        mBarImage = (ImageView) view.findViewById(R.id.bar_photo);
+        mBarId = (TextView) view.findViewById(R.id.bar_list_id);
+        HashMap<String, String> map;
+        map = barsList.get(position);
+
+        String bar_id = map.get(TAG_BAR_ID);
+        String image_url = map.get(TAG_DRINK_IMAGE);
+        mBarId.setText(bar_id);
+
+            Picasso.with(activity.getApplicationContext())
+                    .load(image_url)
+                    .into(mBarImage);
+
+        return view;
+    }
+}
